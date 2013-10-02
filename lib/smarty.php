3951c15b140925e846c8cb5dd548a4753642d031
nyomo <nyomo@oak>
2013-10-02 15:45:07 +0900

    Smartyを使う時の処理が入ったファイル

---
+++ lib/smarty.php
@@ -0,0 +1,143 @@
+<?php
+//ここからcatbodyのコピペ
+	if (! file_exists(SKIN_FILE) || ! is_readable(SKIN_FILE))
+		die_message('SKIN_FILE is not found');
+
+	$_LINK = $_IMAGE = array();
+
+	// Add JavaScript header when ...
+	if ($trackback && $trackback_javascript) $javascript = 1; // Set something If you want
+	if (! PKWK_ALLOW_JAVASCRIPT) unset($javascript);
+
+	$_page  = isset($vars['page']) ? $vars['page'] : '';
+	$r_page = rawurlencode($_page);
+
+	// Set $_LINK for skin
+	$_LINK['add']      = "$script?cmd=add&amp;page=$r_page";
+	$_LINK['backup']   = "$script?cmd=backup&amp;page=$r_page";
+	$_LINK['copy']     = "$script?plugin=template&amp;refer=$r_page";
+	$_LINK['diff']     = "$script?cmd=diff&amp;page=$r_page";
+	$_LINK['edit']     = "$script?cmd=edit&amp;page=$r_page";
+	$_LINK['filelist'] = "$script?cmd=filelist";
+	$_LINK['freeze']   = "$script?cmd=freeze&amp;page=$r_page";
+	$_LINK['help']     = "$script?" . rawurlencode($help_page);
+	$_LINK['list']     = "$script?cmd=list";
+	$_LINK['new']      = "$script?plugin=newpage&amp;refer=$r_page";
+	$_LINK['rdf']      = "$script?cmd=rss&amp;ver=1.0";
+	$_LINK['recent']   = "$script?" . rawurlencode($whatsnew);
+	$_LINK['refer']    = "$script?plugin=referer&amp;page=$r_page";
+	$_LINK['reload']   = "$script?$r_page";
+	$_LINK['rename']   = "$script?plugin=rename&amp;refer=$r_page";
+	$_LINK['rss']      = "$script?cmd=rss";
+	$_LINK['rss10']    = "$script?cmd=rss&amp;ver=1.0"; // Same as 'rdf'
+	$_LINK['rss20']    = "$script?cmd=rss&amp;ver=2.0";
+	$_LINK['search']   = "$script?cmd=search";
+	$_LINK['top']      = "$script?" . rawurlencode($defaultpage);
+	if ($trackback) {
+		$tb_id = tb_get_id($_page);
+		$_LINK['trackback'] = "$script?plugin=tb&amp;__mode=view&amp;tb_id=$tb_id";
+	}
+	$_LINK['unfreeze'] = "$script?cmd=unfreeze&amp;page=$r_page";
+	$_LINK['upload']   = "$script?plugin=attach&amp;pcmd=upload&amp;page=$r_page";
+
+	// Init flags
+	$is_page = (is_pagename($_page) && ! arg_check('backup') && $_page != $whatsnew);
+	$is_read = (arg_check('read') && is_page($_page));
+	$is_freeze = is_freeze($_page);
+
+	// Last modification date (string) of the page
+	$lastmodified = $is_read ?  format_date(get_filetime($_page)) .
+		' ' . get_pg_passage($_page, FALSE) : '';
+
+	// List of attached files to the page
+	$attaches = ($attach_link && $is_read && exist_plugin_action('attach')) ?
+		attach_filelist() : '';
+
+	// List of related pages
+	$related  = ($related_link && $is_read) ? make_related($_page) : '';
+
+	// List of footnotes
+	ksort($foot_explain, SORT_NUMERIC);
+	$notes = ! empty($foot_explain) ? $note_hr . join("\n", $foot_explain) : '';
+
+	// Tags will be inserted into <head></head>
+	$head_tag = ! empty($head_tags) ? join("\n", $head_tags) ."\n" : '';
+
+	// 1.3.x compat
+	// Last modification date (UNIX timestamp) of the page
+	$fmt = $is_read ? get_filetime($_page) + LOCALZONE : 0;
+
+	// Search words
+	if ($search_word_color && isset($vars['word'])) {
+		$body = '<div class="small">' . $_msg_word . htmlspecialchars($vars['word']) .
+			'</div>' . $hr . "\n" . $body;
+
+		// BugTrack2/106: Only variables can be passed by reference from PHP 5.0.5
+		// with array_splice(), array_flip()
+		$words = preg_split('/\s+/', $vars['word'], -1, PREG_SPLIT_NO_EMPTY);
+		$words = array_splice($words, 0, 10); // Max: 10 words
+		$words = array_flip($words);
+
+		$keys = array();
+		foreach ($words as $word=>$id) $keys[$word] = strlen($word);
+		arsort($keys, SORT_NUMERIC);
+		$keys = get_search_words(array_keys($keys), TRUE);
+		$id = 0;
+		foreach ($keys as $key=>$pattern) {
+			$s_key    = htmlspecialchars($key);
+			$pattern  = '/' .
+				'<textarea[^>]*>.*?<\/textarea>' .	// Ignore textareas
+				'|' . '<[^>]*>' .			// Ignore tags
+				'|' . '&[^;]+;' .			// Ignore entities
+				'|' . '(' . $pattern . ')' .		// $matches[1]: Regex for a search word
+				'/sS';
+			$decorate_Nth_word = create_function(
+				'$matches',
+				'return (isset($matches[1])) ? ' .
+					'\'<strong class="word' .
+						$id .
+					'">\' . $matches[1] . \'</strong>\' : ' .
+					'$matches[0];'
+			);
+			$body  = preg_replace_callback($pattern, $decorate_Nth_word, $body);
+			$notes = preg_replace_callback($pattern, $decorate_Nth_word, $notes);
+			++$id;
+		}
+	}
+
+	$longtaketime = getmicrotime() - MUTIME;
+	$taketime     = sprintf('%01.03f', $longtaketime);
+//ここまでcatbody
+
+//ここからsmartyで出力するための内容
+$smarty->assign('OutputEncoding',SYSTEM_ENCODING);
+if (arg_check('read') && exist_plugin_convert('menu')) { 
+	$menubar = do_plugin_convert('menu');
+}else{
+	$menubar = '';
+}
+$smarty->configLoad('smarty.conf');
+$toollist = explode(' ',$smarty->getConfigVars('ToolBar'));
+$toolbar = array();
+foreach($toollist as $item){
+	if(isset($_LINK[$item]))
+	$toolbar[$item] = $_LINK[$item];
+}
+$smarty->assign('MenuBody',$menubar);
+$smarty->assign('PageTitle',$vars['page']);
+$smarty->assign('WikiBody',$body);
+$smarty->assign('FootNote',$notes);
+require_once(PLUGIN_DIR . 'topicpath.inc.php');
+$topicpath = plugin_topicpath_inline(); 
+$smarty->assign('TopicPath',$topicpath);
+$smarty->assign('SiteTop','');
+$smarty->assign('ImgURL','');
+$smarty->assign('LastModified',$lastmodified);
+$smarty->assign('ConvertTime',$taketime);
+$smarty->assign('ToolBarItem',$toolbar);
+$smarty->assign('ToolBarIcon',$_IMAGE);
+
+$smarty->display('index.tpl');
+
+
+?>
