2f48943747a1804a8232de3bd8d2527f212822ee
nyomo <nyomo@oak>
2013-10-12 13:14:06 +0900

    よくやる文字コード変換を関数にした

--- lib/func.php
+++ lib/func.php
@@ -766,4 +766,13 @@ if (! function_exists('sha1')) {
 		}
 	}
 }
+
+function conv_encoding($str,$to=SYSTEM_ENCODING,$from=NULL){
+	if(is_null($from)){
+		$str = mb_convert_encoding($str,$to,mb_detect_encoding($str));
+	}else{
+		$str = mb_convert_encoding($str,$to,$from);
+	}
+	return $str;
+}
 ?>
