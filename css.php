ae0b41c956400015be74e698f5744d503f383eb1
nyomo <nyomo@oak>
2013-11-02 19:30:50 +0900

    とりあえずコピーすれば使える様に修正

--- css.php
+++ css.php
@@ -1,8 +1,9 @@
 <?php
 //pukiwiki本体のディレクトリを指定する
-define('DATA_HOME','./');
+define('DATA_HOME','./pukiwiki/');
+
 date_default_timezone_set('ASIA/Tokyo');
-require_once('pukiwiki.ini.php');
+require_once(DATA_HOME.'pukiwiki.ini.php');
 require_once(SMARTY_DIR.'Smarty.class.php');
 $smarty->configLoad('smarty.conf');
 header('Content-type: text/css;charset="'.SYSTEM_ENCODING.'"');
